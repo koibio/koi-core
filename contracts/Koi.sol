bd67a4202df75f286eab8443adad26c5fb8e73c3
koibio <koibio@protonmail.com>
2021-06-08 15:01:34 +0800

    delete func  burnfrom

--- contracts/Koi.sol
+++ contracts/Koi.sol
@@ -8,18 +8,10 @@ contract Koi is StandardToken {
     uint256 public INITIAL_SUPPLY = 60000000000000000000000000;
     constructor() public{
         totalSupply_ = INITIAL_SUPPLY;
-        totalSupply();
         balances[msg.sender] = INITIAL_SUPPLY;
     }
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
-    }
-
-    function burnFrom(address account, uint256 amount) public {
-        uint256 currentAllowance = allowance(account, msg.sender);
-        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
-        emit Approval(account, msg.sender, currentAllowance - amount);
-        _burn(account, amount);
     }
 
     function _burn(address account, uint256 amount) internal {
